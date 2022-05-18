6edac9fc3fd1b2cf4da3451816b299f0ed8dd663
srabon0 <srabonemam4@gmail.com>
2022-05-19 02:33:42 +0600

    web token verificatin successfull

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 
@@ -16,6 +17,23 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
+
+function verifyAuthToken(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      return res.status(401).send({ message: 'UnAuthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    
+    jwt.verify(token, process.env.ACCESS_TOKEN_ENC_KEY, function (err, decoded) {
+      if (err) {
+        return res.status(403).send({ message: 'Forbidden access' })
+      }
+      req.decoded = decoded;
+      next();
+    });
+  }
+
 
 async function run() {
   try {
@@ -34,24 +52,24 @@ async function run() {
     })
 
     //addTask
-    app.post("/addtask",async(req,res)=>{
+    app.post("/addtask",verifyAuthToken, async(req,res)=>{
         const task = req.body;
-        console.log(task)
+      
         const insertConfirmation = await taskCollection.insertOne(task);
         res.send(insertConfirmation)
     })
     //delete task
-    app.delete('/delete/:id',async(req,res)=>{
+    app.delete('/delete/:id', verifyAuthToken ,async(req,res)=>{
         const id= req.params
-        console.log("delete this", id)
+        
         const query = { _id: ObjectId(id) };
         const result = await taskCollection.deleteOne(query);
         res.send(result)
     })
 
-    app.put('/done/:id',async(req,res)=>{
+    app.put('/done/:id', verifyAuthToken , async(req,res)=>{
         const id= req.params
-        console.log("update", id)
+       
         const filter = { _id: ObjectId(id) };
         const options = { upsert: true };
         const updateDoc = {
@@ -61,7 +79,13 @@ async function run() {
           };
           const result = await taskCollection.updateOne(filter, updateDoc, options);
         res.send(result)
-    })
+    });
+
+    app.post('/login', async (req, res) => {
+        const email = req.body;
+        const token = jwt.sign(email, process.env.ACCESS_TOKEN_ENC_KEY);
+        res.send({ token });
+      });
    
    
   } finally {
